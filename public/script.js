884f30ab2ac8747ccda390ae8244cb25b506c51e
DerGreed <joel.hilberg@gmail.com>
2025-11-27 19:05:47 +0100

    changed api url

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 async function loadUsers() {
   try {
-    const res = await fetch("https://3.64.214.105/prod/users");
+    const res = await fetch("http://3.64.214.105:3000/users");
     const data = await res.json();
     document.getElementById("output").textContent = JSON.stringify(
       data,
